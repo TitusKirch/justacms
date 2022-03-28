3055a77ccbc1df34ad0f1131733143230df6e75a
Titus Kirch <titus.kirch@tkirch.dev>
2022-03-29 01:16:07 +0200

    Add pages

--- apps/frontend/pages/article/[id]-[title].vue
+++ apps/frontend/pages/article/[id]-[title].vue
@@ -23,7 +23,7 @@
             {{ article.attributes.title }}
           </p>
           <div class="mt-3 text-base text-gray-900">
-            <div class="flex gap-2 flex-col" v-html="articleHtml"></div>
+            <div class="flex gap-2 flex-col" v-html="computedHtml" />
           </div>
         </div>
         <div class="mt-6 flex items-center">
@@ -58,8 +58,8 @@
 </template>
 
 <script setup lang="ts">
-import { ArticleResponse } from "~~/types/Article";
 import { JSDOM } from "jsdom";
+import { ArticleResponse } from "~~/types/Article";
 const { findOne } = useStrapi4();
 const config = useRuntimeConfig();
 const route = useRoute();
@@ -115,31 +115,6 @@ const thumbnailUrl = computed(() => {
   return "/images/article/default.jpg";
 });
 
-const articleHtml = computed(() => {
-  if (article && article.value.attributes.message) {
-    let html = article.value.attributes.message;
-
-    const dom = new JSDOM(html);
-
-    // replace all image src and activate lazy loading
-    const images = dom.window.document.querySelectorAll("img");
-    images.forEach((image) => {
-      image.setAttribute("loading", "lazy");
-      image.setAttribute("src", config.STRAPI_URL + image.getAttribute("src"));
-    });
-
-    // add .article-html-link to all links
-    const links = dom.window.document.querySelectorAll("a");
-    links.forEach((link) => {
-      link.classList.add("text-teal-600", "hover:underline");
-    });
-
-    return dom.window.document.body.innerHTML;
-  }
-
-  return "";
-});
-
 const post = ref({
   title: "Boost your conversion rate",
   href: "#",
@@ -157,5 +132,54 @@ const post = ref({
     imageUrl:
       "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
   },
+});
+
+const html = ref(article.value.attributes.message);
+
+const computedHtml = computed(() => {
+  if (!html.value) return "";
+
+  const dom = new JSDOM(html.value);
+
+  // replace all image src and activate lazy loading
+  const images = dom.window.document.querySelectorAll("img");
+  images.forEach((image) => {
+    image.setAttribute("loading", "lazy");
+    image.setAttribute("src", config.STRAPI_URL + image.getAttribute("src"));
+  });
+
+  // modify all links
+  const links = dom.window.document.querySelectorAll("a");
+  links.forEach((link) => {
+    link.classList.add("text-teal-600", "hover:underline");
+  });
+
+  // modify all headings
+  const h1 = dom.window.document.querySelectorAll("h1");
+  h1.forEach((h) => {
+    h.classList.add("text-4xl", "font-semibold");
+  });
+  const h2 = dom.window.document.querySelectorAll("h2");
+  h2.forEach((h) => {
+    h.classList.add("text-3xl", "font-semibold");
+  });
+  const h3 = dom.window.document.querySelectorAll("h3");
+  h3.forEach((h) => {
+    h.classList.add("text-2xl", "font-medium");
+  });
+  const h4 = dom.window.document.querySelectorAll("h4");
+  h4.forEach((h) => {
+    h.classList.add("text-xl", "font-medium");
+  });
+  const h5 = dom.window.document.querySelectorAll("h5");
+  h5.forEach((h) => {
+    h.classList.add("text-lg", "font-normal");
+  });
+  const h6 = dom.window.document.querySelectorAll("h6");
+  h6.forEach((h) => {
+    h.classList.add("text-base", "font-normal");
+  });
+
+  return dom.window.document.body.innerHTML;
 });
 </script>
